68234c7a2bdca637b245566709a83708a4aa273a
mthom26 <mthom2626@gmail.com>
2019-11-18 15:57:01 +0000

    Update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,4 +8,18 @@ require("dotenv").config();
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Per Gouras',
+        short_name: 'Per Gouras',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'src/assets/images/icon.svg'
+      }
+    }
+  ]
 };
